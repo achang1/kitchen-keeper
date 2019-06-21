336142ec8d21f195e60cad46419695dc0169e494
Min-Ji Han <minjihan@msn.com>
2019-06-20 21:46:11 -0700

    Created test folder.

--- python-django/inventory/schema.py
+++ python-django/inventory/schema.py
@@ -6,6 +6,5 @@ from inventory.operations.item_operations import Query as ItemQuery, Mutation as
 class Query(UserQuery, StorageQuery, ItemQuery, graphene.ObjectType):
     pass
 
-
 class Mutation(UserMutation, StorageMutation, ItemMutation, graphene.ObjectType):
     pass
